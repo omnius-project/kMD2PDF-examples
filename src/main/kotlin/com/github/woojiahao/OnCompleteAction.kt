52f7581021ccb65761cb687177fb7fc7f8325afb
woojiahao <woojiahao1234@gmail.com>
2019-02-21 10:46:00 +0800

    Added onComplete example

---
+++ src/main/kotlin/com/github/woojiahao/OnCompleteAction.kt
@@ -0,0 +1,24 @@
+package com.github.woojiahao
+
+import java.awt.Desktop
+import java.io.File
+
+/**
+ * This example shows how the [MarkdownDocument.onComplete] method can be used to launch the exported PDF on any OS.
+ *
+ * [MarkdownDocument.onComplete] passes the exported PDF [File] so that users can manipulate it as they see fit.
+ */
+fun main() {
+  val markdownDocument = MarkdownDocument("resources/markdown-all-in-one.md")
+  markdownDocument.onComplete {
+    println("Conversion success - opening document")
+
+    // Open the PDF document
+    if (Desktop.isDesktopSupported()) {
+      Desktop.getDesktop().open(it)
+    } else {
+      System.out.println("Awt Desktop is not supported!")
+    }
+  }
+  markdownDocument.toPDF()
+}
