02d5cc36780f87507abcfefc2575a5f91622fc80
woojiahao <woojiahao1234@gmail.com>
2019-02-21 10:40:55 +0800

    Added basic exmaples

---
+++ src/main/kotlin/com/github/woojiahao/DefaultStyling.kt
@@ -0,0 +1,14 @@
+package com.github.woojiahao
+
+import com.github.woojiahao.style.Style
+
+/**
+ * This example showcases the most bare-bones use of the library.
+ *
+ * If no [Style] is provided, it uses the default styling and exports to the same location where the original markdown
+ * document was located.
+ */
+fun main() {
+  val markdownDocument = MarkdownDocument("resources/markdown-all-in-one.md")
+  markdownDocument.toPDF()
+}
---
+++ src/main/kotlin/com/github/woojiahao/SpecifyingPDFLocation.kt
@@ -0,0 +1,12 @@
+package com.github.woojiahao
+
+/**
+ * This example shows how a user can specify the export location of the PDF.
+ *
+ * **Note:** The exported PDF location must end with the PDF name and with the `.pdf` extension, otherwise, it will
+ * throw an error.
+ */
+fun main() {
+  val markdownDocument = MarkdownDocument("resources/markdown-all-in-one.md")
+  markdownDocument.toPDF("${System.getProperty("user.home")}/Desktop/exported.pdf")
+}
