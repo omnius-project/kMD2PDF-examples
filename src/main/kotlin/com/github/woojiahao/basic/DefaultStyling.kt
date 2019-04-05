b0f7193353fe8e1e4acbd36b22b64e370a2c4aa1
woojiahao <woojiahao1234@gmail.com>
2019-04-05 16:27:39 +0800

    Updated barebones example

--- src/main/kotlin/com/github/woojiahao/basic/DefaultStyling.kt
+++ src/main/kotlin/com/github/woojiahao/basic/DefaultStyling.kt
@@ -1,15 +1,15 @@
 package com.github.woojiahao.basic
 
 import com.github.woojiahao.MarkdownDocument
-import com.github.woojiahao.style.Style
+import com.github.woojiahao.markdownConverter
 
 /**
  * This example showcases the most bare-bones use of the library.
- *
- * If no [Style] is provided, it uses the default styling and exports to the same location where the original markdown
- * document was located.
  */
 fun main() {
-  val markdownDocument = MarkdownDocument("resources/markdown-all-in-one.md")
-  markdownDocument.toPDF()
+  val document = MarkdownDocument("resources/markdown-all-in-one.md")
+  val converter = markdownConverter {
+    document(document)
+  }
+  converter.convert()
 }
